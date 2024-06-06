fb999a8e981f2d090ff0acc5c0dc6b137b67a42a
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-06-06 10:55:48 +0200

    log_in.py
    log in creating, and later add json import

---
+++ log_in.py
@@ -0,0 +1,23 @@
+import json 
+
+class Login:
+  
+  
+  def __init__(self, username, pasword):
+    self.name = username
+    self.pasword = password 
+
+  def name(self):
+    if username in data:
+      print("You have correct username")
+    elif len(username) <= 6:
+      print("you have short username")
+
+  username = input("Enter your username:\n")
+
+  def pasword(self):
+    if password not in username:
+      return username 
+    else:
+      print("you are succesfull loged")
+  
