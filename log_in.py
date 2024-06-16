cde4aab2b2b07317d027fe91885d2c14a8024eef
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-06-16 09:38:30 +0200

    Update log_in.py
    change login

--- log_in.py
+++ log_in.py
@@ -1,37 +1,46 @@
 import json
 
 
-class Login:
+class User:
     def __init__(self, username, password, data):
         self.username = username
         self.password = password
         self.data = data
 
-    @staticmethod
     def load(file_path):
         with open("ID2.json","r") as file:
             data = file.read()
-            objekt = json.loads(data)
-            print(objekt)
+            return data["users"]
     
     def first_part(self,entered_username):
         if len(entered_username) <= 6:
             print("you have short username!")
             return entered_username
     
-    def second_part(self,entered_pasword,entered_username):
-        if len(entered_pasword) <= 8:
+    def second_part(self,entered_password,entered_username):
+        if len(entered_password) <= 8:
             print("You have short pasword!")
             return entered_username
 
-    def verify(self, entered_username, entered_password):
-        if entered_username != self.username:
-            print("Incorrect username")
-        elif entered_password != self.password:
-            print("Incorrect password")
-        else:
-            print("You are successfully logged in")
+    def login(self, entered_username, entered_password):
+        for users in self.data:
+            if users['username'] == entered_username:
+                if users['password'] == entered_password:
+                    print("You are successfully logged in")
+                    return True
+                else:
+                    print("Incorrect password")
+                    return False
+        print("Incorrect username")
+        return False
+    
+
+
+
+   
+ 
 
 # Získání uživatelského jména a hesla od uživatele
 entered_username = input("Write your username: ")
 entered_password = input("Write your password: ")
+
