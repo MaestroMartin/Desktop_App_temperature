5831845b2f2b81eab7db75e4e36dcbf569b81bb3
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-06-19 11:11:16 +0200

    gui.py
    add Registration

--- gui.py
+++ gui.py
@@ -15,15 +15,17 @@ class Color(QWidget):
         self.setpalette(paletta)
 
 
-class my_window(QMainWindow):
+class Login_window(QMainWindow):
     def __init__(self):
         super(my_window,self).__init__()
         self.setGeometry(1200, 300,700,700)
         self.setWindowTitle("Martin_first")
         self.setToolTip("Cursor")
         self.initUI()
+        self.Login()
+        
 
-    def initUI(self):
+    def initUI(self,Login):
         self.username = QtWidgets.QLabel(self)
         self.username.setText("Enter your username: ")
         self.username.move(50,50)
@@ -45,6 +47,46 @@ class my_window(QMainWindow):
         self.btn_save = QtWidgets.QPushButton(self)
         self.btn_save.setText("Login")
         self.btn_save.move(200,130)
+
+class Registration_window(QMainWindow):
+    def __init__(self):
+        super(Registration_window,self).__init()
+        self.setGeometry(1200, 300,700,700)
+        self.setWindowTitle("Martin_first")
+        self.setToolTip("Cursor")
+        self.initUI()
+        self.Registration()
+
+    def initUI(self):
+
+        self.entered_username = QtWidgets.Qlabel(self)
+        self.entered_username.setText("Enter your username: ")
+        self.entered_username.move(50, 50)
+        self.entered_username.resize(250,32)
+
+        self.entered_password= QtWidgets.QLabel(self)
+        self.entered_password.setText("Enter your pasword: ")
+        self.entered_password.move(50, 90)
+        self.entered_password.resize(250,32)
+
+        self.entered_again_password = QtWidgets.QLabel(self)
+        self.entered_again_password.setText("Enter you password again: ")
+        self.entered_again_password.move(50, 130)
+        self.entered_again_password.resize(250, 32)         
+                
+        self.txt_entered_username = QtWidgets.QLineEdit(self)
+        self.txt_entered_username.move(200,90)
+        self.txt_entered_username.resize(250,32)
+
+        self.txt_entered_password = QtWidgets.QLineEdit(self)
+        self.txt_entered_password.move(200, 90)
+        self.txt_entered_password.resize(250,32)
+       
+        self.txt_entered_again_password = QtWidgets.QLineEdit(self)
+        self.txt_entered_again_password.move(200,90)
+        self.txt_entered_again_password.resize(250,32)
+        
+        
 
 
 def window():
