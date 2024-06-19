cc2933faf044fdf89eb5f4e13c1cc15f1acb47d0
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-06-19 12:11:22 +0200

    gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow,QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow,QWidget,QVBoxLayout
 from PyQt6.QtGui import QIcon
 from PyQt6.QtGui import QPalette, QColor
 from log_in import Login
@@ -15,9 +15,17 @@ class Color(QWidget):
         self.setpalette(paletta)
 
 
-class Login_window(QMainWindow):
+        self.layout = QVBoxLayout()
+        self.label = QLabel("Entered_Username")
+        self.username = QLineEdit()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.entered_username)
+
+
+
+class LoginWindow(QWidget):
     def __init__(self):
-        super(my_window,self).__init__()
+        super(LoginWindow,self).__init__(parent=None)
         self.setGeometry(1200, 300,700,700)
         self.setWindowTitle("Martin_first")
         self.setToolTip("Cursor")
@@ -26,29 +34,51 @@ class Login_window(QMainWindow):
         
 
     def initUI(self,Login):
-        self.username = QtWidgets.QLabel(self)
-        self.username.setText("Enter your username: ")
-        self.username.move(50,50)
+        self.entered_username = QtWidgets.QLabel(self)
+        self.entered_username.setText("Enter your username: ")
+        self.entered_username.move(50,50)
         self.username.resize(200, 32)
 
-        self.pasword = QtWidgets.QLabel(self)
-        self.pasword.setText("Enter your password:")
-        self.pasword.move(50, 90)
-        self.pasword.resize(200, 32)
+        self.entered_pasword = QtWidgets.QLabel(self)
+        self.entered_pasword.setText("Enter your password:")
+        self.entered_pasword.move(50, 90)
+        self.entered_pasword.resize(200, 32)
 
-        self.txt_username = QtWidgets.QLineEdit(self)
-        self.txt_username.move(200, 50)
-        self.txt_username.resize(250, 32)
+        self.txt_entered_username = QtWidgets.QLineEdit(self)
+        self.txt_entered_username.move(200, 50)
+        self.txt_entered_username.resize(250, 32)
 
-        self.txt_pasword = QtWidgets.QLineEdit(self)
-        self.txt_pasword.move(200, 90)
-        self.txt_pasword.resize(250,32)
+        self.txt_entered_pasword = QtWidgets.QLineEdit(self)
+        self.txt_entered_pasword.move(200, 90)
+        self.txt_entered_pasword.resize(250,32)
+        self._entered_password.setEchoMode(QLineEdit.EchoMode.entered_Password)
 
         self.btn_save = QtWidgets.QPushButton(self)
         self.btn_save.setText("Login")
         self.btn_save.move(200,130)
 
-class Registration_window(QMainWindow):
+        self.register_button = QPushButton("Register")
+        self.layout.addWidget(self.register_button)
+
+        self.setLayout(self.layout)
+
+        self.login_button.clicked.connect(self.handle_login)
+        self.register_button.clicked.connect(self.open_registration)
+        
+        def handle_login(self):
+        username = self.username.text()
+        password = self.password.text()
+        if login(username, password):
+            print("Login successful")
+        else:
+            print("Login failed")
+
+        def open_registration(self):
+        self.registration_window = RegistrationWindow(self)
+        self.registration_window.show()
+
+
+class RegistrationWindow(QMainWindow):
     def __init__(self):
         super(Registration_window,self).__init()
         self.setGeometry(1200, 300,700,700)
